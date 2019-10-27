ce1054ecfb33a0f26518e296c09b153cccc28e52
Jerry Kim <jerryrox@live.co.kr>
2019-10-27 22:41:38 +1300

    Create ZipCompressedTest.cs
    - Need to implement Uncompress test case.

---
+++ Framework/IO/Compressed/ZipCompressedTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace PBFramework.IO.Compressed.Tests
+{
+    public class ZipCompressedTest {
+
+        [Test]
+        public void TestSource()
+        {
+            var fileInfo = GetFileInfo();
+            var zip = new ZipCompressed(fileInfo);
+            Assert.AreEqual(fileInfo, zip.Source);
+        }
+
+        [Test]
+        public void TestGetUncompressedSize()
+        {
+            var fileInfo = GetFileInfo();
+            var zip = new ZipCompressed(fileInfo);
+            long size = zip.GetUncompressedSize();
+
+            Debug.Log("Returned size: " + size);
+        }
+        
+        private FileInfo GetFileInfo()
+        {
+            string path = Path.Combine(Application.streamingAssetsPath, "TestZip.zip");
+            var source = new FileInfo(path);
+            Assert.IsTrue(source.Exists);
+            return source;
+        }
+    }
+}
