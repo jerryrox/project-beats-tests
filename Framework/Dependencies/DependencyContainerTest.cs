034dbe198c8bb3ac0da98627cf2f459683a9d99e
Jerry Kim <jerryrox@live.co.kr>
2019-11-04 16:21:01 +1300

    Added test codes for dependency module

---
+++ Framework/Dependencies/DependencyContainerTest.cs
@@ -0,0 +1,179 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace PBFramework.Dependencies.Tests
+{
+    public class DependencyContainerTest {
+        
+        [Test]
+        public void TestCache()
+        {
+            var dependency = new DependencyContainer();
+
+            try
+            {
+                dependency.Cache(null);
+            }
+            catch (ArgumentNullException e)
+            {
+            }
+
+            Dummy dummy = new Dummy();
+            Dummy dummy2 = new Dummy();
+
+            dependency.Cache(dummy);
+            Assert.AreSame(dummy, dependency.Get<Dummy>());
+
+            LogAssert.Expect(LogType.Warning, $"DependencyContainer.CacheAs - A dependency already exists for type ({typeof(Dummy).Name})!");
+            dependency.Cache(dummy2);
+
+            dependency.Cache(dummy2, true);
+            Assert.AreSame(dummy2, dependency.Get<Dummy>());
+            Assert.AreNotSame(dummy2, dummy);
+        }
+
+        [Test]
+        public void TestCacheAs()
+        {
+            var dependency = new DependencyContainer();
+
+            try
+            {
+                dependency.CacheAs<Dummy>(null);
+            }
+            catch (ArgumentNullException e)
+            {
+            }
+
+            Dummy dummy = new Dummy();
+            Dummy dummy2 = new Dummy();
+
+            dependency.CacheAs(dummy);
+            Assert.AreSame(dummy, dependency.Get<Dummy>());
+
+            dependency.CacheAs<IDummy>(dummy);
+            Assert.AreSame(dummy, dependency.Get<IDummy>());
+
+            LogAssert.Expect(LogType.Warning, $"DependencyContainer.CacheAs - A dependency already exists for type ({typeof(Dummy).Name})!");
+            dependency.CacheAs(dummy2);
+
+            LogAssert.Expect(LogType.Warning, $"DependencyContainer.CacheAs - A dependency already exists for type ({typeof(IDummy).Name})!");
+            dependency.CacheAs<IDummy>(dummy2);
+
+            dependency.CacheAs(dummy2, true);
+            Assert.AreSame(dummy2, dependency.Get<Dummy>());
+            Assert.AreNotSame(dummy2, dummy);
+
+            dependency.CacheAs<IDummy>(dummy2, true);
+            Assert.AreSame(dummy2, dependency.Get<IDummy>());
+            Assert.AreNotSame(dummy2, dummy);
+        }
+
+        [Test]
+        public void TestCacheFrom()
+        {
+            var dependency = new DependencyContainer();
+            var newDependency = new DependencyContainer();
+
+            List<int> a = new List<int>();
+            List<float> b = new List<float>();
+
+            dependency.Cache(a);
+            dependency.Cache(b);
+            Assert.AreSame(a, dependency.Get<List<int>>());
+            Assert.AreSame(b, dependency.Get<List<float>>());
+
+            newDependency.CacheFrom(dependency);
+            Assert.AreSame(dependency.Get<List<int>>(), newDependency.Get<List<int>>());
+            Assert.AreSame(dependency.Get<List<float>>(), newDependency.Get<List<float>>());
+
+            var newA = new List<int>();
+            var newB = new List<float>();
+            Assert.AreNotSame(newA, dependency.Get<List<int>>());
+            Assert.AreNotSame(newA, newDependency.Get<List<int>>());
+            Assert.AreNotSame(newB, dependency.Get<List<float>>());
+            Assert.AreNotSame(newB, newDependency.Get<List<float>>());
+
+            var lastDependency = new DependencyContainer();
+            lastDependency.Cache(newA);
+            lastDependency.Cache(newB);
+
+            newDependency.CacheFrom(lastDependency);
+            Assert.AreNotSame(newA, newDependency.Get<List<int>>());
+            Assert.AreNotSame(newB, newDependency.Get<List<float>>());
+
+            newDependency.CacheFrom(lastDependency, true);
+            Assert.AreSame(newA, newDependency.Get<List<int>>());
+            Assert.AreSame(newB, newDependency.Get<List<float>>());
+        }
+
+        [Test]
+        public void TestClone()
+        {
+            var dependency = new DependencyContainer();
+
+            Dummy dummy = new Dummy();
+            dependency.Cache(dummy);
+            dependency.CacheAs<IDummy>(dummy);
+
+            var cloned = dependency.Clone();
+            Assert.IsNotNull(cloned);
+            Assert.AreSame(dependency.Get<Dummy>(), cloned.Get<Dummy>());
+            Assert.AreSame(dependency.Get<IDummy>(), cloned.Get<IDummy>());
+        }
+
+        [Test]
+        public void TestRemove()
+        {
+            var dependency = new DependencyContainer();
+
+            Dummy dummy = new Dummy();
+            dependency.Cache(dummy);
+            dependency.CacheAs<IDummy>(dummy);
+
+            Assert.AreSame(dummy, dependency.Get<Dummy>());
+            Assert.AreSame(dummy, dependency.Get<IDummy>());
+
+            dependency.Remove(dummy);
+            Assert.IsNull(dependency.Get<Dummy>());
+            Assert.AreSame(dummy, dependency.Get<IDummy>());
+
+            dependency.Remove<IDummy>();
+            Assert.IsNull(dependency.Get<Dummy>());
+            Assert.IsNull(dependency.Get<IDummy>());
+        }
+
+        [Test]
+        public void TestGet()
+        {
+            var dependency = new DependencyContainer();
+
+            Dummy dummy = new Dummy();
+            dependency.Cache(dummy);
+            dependency.CacheAs<IDummy>(dummy);
+
+            Assert.AreSame(dummy, dependency.Get<Dummy>());
+            Assert.AreSame(dummy, dependency.Get<IDummy>());
+            Assert.AreSame(dummy, dependency.Get(typeof(Dummy)));
+            Assert.AreSame(dummy, dependency.Get(typeof(IDummy)));
+
+            LogAssert.Expect(LogType.Warning, $"DependencyContainer.Get - Failed to find dependency of type: {typeof(string).Name}");
+            Assert.IsNull(dependency.Get<string>());
+            LogAssert.Expect(LogType.Warning, $"DependencyContainer.Get - Failed to find dependency of type: {typeof(string).Name}");
+            Assert.IsNull(dependency.Get(typeof(string)));
+        }
+
+        private interface IDummy
+        {
+
+        }
+
+        private class Dummy : IDummy
+        {
+        }
+    }
+}
---
+++ Framework/Dependencies/TypeInjectorTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace PBFramework.Dependencies.Tests
+{
+    public class TypeInjectorTest {
+        
+        [Test]
+        public void TestCreation()
+        {
+            TypeInjector injector = new TypeInjector(typeof(Dummy));
+            Assert.IsNull(injector.BaseInjector);
+
+            TypeInjector injector2 = new TypeInjector(typeof(Dummy2), injector);
+            Assert.IsNotNull(injector2.BaseInjector);
+        }
+
+        [Test]
+        public void TestInject()
+        {
+            IList<int> list1 = new List<int>();
+            List<float> list2 = new List<float>();
+            Test test = new Test();
+
+            var dependency = new DependencyContainer();
+            dependency.Cache(list1);
+            dependency.Cache(list2);
+            dependency.CacheAs<IList<float>>(list2);
+            dependency.Cache(test);
+            dependency.CacheAs<ITest>(test);
+
+            TypeInjector injector = new TypeInjector(typeof(Dummy));
+            TypeInjector injector2 = new TypeInjector(typeof(Dummy2), injector);
+            TypeInjector injector3 = new TypeInjector(typeof(EmptyDummy));
+
+            var dummy = new Dummy();
+            var dummy2 = new Dummy2();
+            var dummy3 = new EmptyDummy();
+
+            LogAssert.Expect(LogType.Error, $"TypeInjector.Inject - Injection target's type ({typeof(Dummy2).Name}) does not match the responsible type ({typeof(Dummy).Name})!");
+            injector.Inject(dummy2, dependency);
+            Assert.IsNull(dummy2.List1);
+            Assert.IsNull(dummy2.List2);
+            Assert.IsNull(dummy2.List3);
+            Assert.IsNull(dummy2.TestClass);
+            Assert.IsNull(dummy2.TestInterface);
+
+            injector.Inject(dummy, dependency);
+            Assert.AreSame(dummy.TestInterface, test);
+            Assert.AreSame(dummy.List1, list1);
+            Assert.AreSame(dummy.List2, list2);
+
+            injector2.Inject(dummy2, dependency);
+            Assert.AreSame(dummy2.TestClass, test);
+            Assert.AreSame(dummy2.TestInterface, test);
+            Assert.AreSame(dummy2.List1, list1);
+            Assert.AreSame(dummy2.List2, list2);
+            Assert.AreSame(dummy2.List3, list2);
+
+            Assert.IsFalse(dummy3.IsCalled);
+            injector3.Inject(dummy3, dependency);
+            Assert.IsTrue(dummy3.IsCalled);
+        }
+
+        private interface ITest
+        {
+            string A { get; }
+            int B { get; }
+        }
+
+        private class Test : ITest
+        {
+            public string A { get; set; }
+            public int B { get; set; }
+        }
+
+        private class EmptyDummy
+        {
+            public bool IsCalled;
+
+            [InitWithDependency]
+            void Init()
+            {
+                IsCalled = true;
+            }
+        }
+
+        private class Dummy
+        {
+            public ITest TestInterface;
+
+            [ReceivesDependency]
+            public List<int> List1 { get; set; }
+
+            [ReceivesDependency]
+            public IList<float> List2 { get; set; }
+
+            [InitWithDependency]
+            void Init(ITest test)
+            {
+                TestInterface = test;
+            }
+        }
+
+        private class Dummy2 : Dummy
+        {
+            public Test TestClass;
+
+            [ReceivesDependency]
+            public List<float> List3 { get; set; }
+
+            [InitWithDependency]
+            void Init(Test test)
+            {
+                TestClass = test;
+            }
+        }
+    }
+}
