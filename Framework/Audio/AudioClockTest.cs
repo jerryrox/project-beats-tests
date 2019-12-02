08ed530285d529287371a69cdeda99361c1d2e01
Jerry Kim <jerryrox@live.co.kr>
2019-12-02 16:49:26 +1300

    Added AudioClockTest & UnityAudioControllerTest

---
+++ Framework/Audio/AudioClockTest.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace PBFramework.Audio.Tests
+{
+    public class AudioClockTest {
+
+        private const float Delta = 100;
+
+
+        [Test]
+        public void TestEvents()
+        {
+            DummyController controller = new DummyController();
+            var clock = controller.Clock;
+            Assert.IsNotNull(clock);
+
+            Assert.IsFalse(clock.IsRunning);
+            controller.MountAudio(new DummyAudio());
+            Assert.IsTrue(clock.IsRunning);
+            controller.MountAudio(null);
+            Assert.IsFalse(clock.IsRunning);
+
+            Assert.IsFalse(clock.IsPlaying);
+            controller.Play();
+            Assert.AreEqual(0f, clock.CurrentTime, Delta);
+            Assert.IsTrue(clock.IsPlaying);
+            controller.Play(2000f);
+            Assert.AreEqual(0f, clock.CurrentTime, Delta);
+
+            controller.Stop();
+            Assert.IsFalse(clock.IsPlaying);
+            Assert.IsFalse(clock.IsPaused);
+            Assert.IsTrue(clock.IsStopped);
+
+            controller.Play(2000f);
+            Assert.AreEqual(-2000f, clock.CurrentTime, Delta);
+            Assert.IsTrue(clock.IsPlaying);
+            Assert.IsFalse(clock.IsPaused);
+            Assert.IsFalse(clock.IsStopped);
+
+            controller.Pause();
+            Assert.AreEqual(-2000f, clock.CurrentTime, Delta);
+            Assert.IsFalse(clock.IsPlaying);
+            Assert.IsTrue(clock.IsPaused);
+            Assert.IsFalse(clock.IsStopped);
+
+            controller.Stop();
+            Assert.AreEqual(0f, clock.CurrentTime, Delta);
+
+            Assert.AreEqual(1f, clock.Rate);
+            controller.SetTempo(2f);
+            Assert.AreEqual(2f, clock.Rate, Delta);
+
+            Assert.AreEqual(0f, clock.CurrentTime, Delta);
+            controller.Seek(5000f);
+            Assert.AreEqual(10000f, clock.CurrentTime, Delta);
+            controller.SetTempo(1f);
+            Assert.AreEqual(5000, clock.CurrentTime, Delta);
+
+            clock.Offset = 1000f;
+            Assert.AreEqual(6000f, clock.CurrentTime, Delta);
+        }
+
+        private class DummyAudio : IAudio
+        {
+            public float Duration { get; set; } = 1000;
+
+            public int Frequency { get; set; } = 22050;
+
+            public int Channels { get; set; } = 2;
+
+            public void Dispose() { }
+        }
+
+        private class DummyController : IMusicController
+        {
+            public event Action<IAudio> OnMounted;
+
+            public event Action<float> OnPlay;
+
+            public event Action<float> OnUnpause;
+
+            public event Action OnPause;
+
+            public event Action OnStop;
+
+            public event Action<float> OnSeek;
+
+            public event Action OnEnd;
+
+            public event Action OnLoop;
+
+            public event Action<float> OnTempo;
+
+            public event Action<float> OnFaded;
+
+
+            public AudioClock Clock { get; }
+
+            public IAudio Audio { get; private set; }
+
+            public bool IsPlaying => Clock.IsPlaying;
+
+            public bool IsPaused => Clock.IsPaused;
+
+            public bool IsStopped => Clock.IsStopped;
+
+            public float Volume => 1f;
+
+            public float LoopTime { get; set; } = 1f;
+
+            public float CurrentTime { get; set; } = 0f;
+
+            public bool IsLoop { get; set; } = false;
+
+
+            public DummyController()
+            {
+                Clock = new AudioClock(this);
+            }
+
+            public void MountAudio(IAudio audio)
+            {
+                Audio = audio;
+                OnMounted?.Invoke(audio);
+            }
+
+            public void Play() => OnPlay?.Invoke(0);
+
+            public void Play(float delay) => OnPlay?.Invoke(-delay);
+
+            public void Unpause() => OnUnpause?.Invoke(CurrentTime);
+
+            public void Pause() => OnPause?.Invoke();
+
+            public void Stop() => OnStop?.Invoke();
+
+            public void Seek(float time) => OnSeek?.Invoke(time);
+
+            public void SetVolume(float volume) { }
+
+            public void Fade(float from, float to) { }
+
+            public void Fade(float to) { }
+
+            public void SetFade(float fade) { }
+
+            public void SetTempo(float tempo) => OnTempo?.Invoke(tempo);
+
+            public void Dispose() { }
+        }
+    }
+}
---
+++ Framework/Audio/UnityAudioControllerTest.cs
@@ -0,0 +1,227 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using PBFramework.Networking;
+
+namespace PBFramework.Audio.Tests
+{
+    public class UnityAudioControllerTest {
+
+        private const float Delta = 100;
+
+
+        [UnityTest]
+        public IEnumerator TestMount()
+        {
+            IAudio audio = null;
+            yield return LoadAudio(a => audio = a);
+
+            var controller = CreateController();
+
+            Assert.IsNull(controller.Audio);
+            controller.MountAudio(audio);
+            Assert.IsNotNull(controller.Audio);
+            Assert.AreEqual(audio, controller.Audio);
+        }
+
+        [UnityTest]
+        public IEnumerator TestPlay()
+        {
+            IAudio audio = null;
+            yield return LoadAudio(a => audio = a);
+
+            var controller = CreateController();
+            controller.MountAudio(audio);
+            controller.Play();
+            Assert.IsTrue(controller.IsPlaying);
+            Assert.IsFalse(controller.IsPaused);
+            Assert.IsFalse(controller.IsStopped);
+            while (controller.IsPlaying)
+            {
+                // Debug.Log("Time: " + controller.CurrentTime);
+                yield return null;
+            }
+            yield return new WaitForSeconds(audio.Duration / 1000f);
+            Assert.AreEqual(0f, controller.CurrentTime, Delta);
+            Assert.IsFalse(controller.IsPlaying);
+            Assert.IsFalse(controller.IsPaused);
+            Assert.IsTrue(controller.IsStopped);
+        }
+
+        [UnityTest]
+        public IEnumerator TestPlayDelay()
+        {
+            IAudio audio = null;
+            yield return LoadAudio(a => audio = a);
+
+            var controller = CreateController();
+            controller.MountAudio(audio);
+            controller.Play(2000);
+            Assert.IsTrue(controller.IsPlaying);
+            Assert.IsFalse(controller.IsPaused);
+            Assert.IsFalse(controller.IsStopped);
+            yield return new WaitForSeconds(audio.Duration / 1000f);
+            Assert.Less(controller.CurrentTime, audio.Duration);
+            Assert.IsTrue(controller.IsPlaying);
+            Assert.IsFalse(controller.IsPaused);
+            Assert.IsFalse(controller.IsStopped);
+            yield return new WaitForSeconds(2f);
+            Assert.AreEqual(0f, controller.CurrentTime, Delta);
+            Assert.IsFalse(controller.IsPlaying);
+            Assert.IsFalse(controller.IsPaused);
+            Assert.IsTrue(controller.IsStopped);
+        }
+
+        [UnityTest]
+        public IEnumerator TestPause()
+        {
+            IAudio audio = null;
+            yield return LoadAudio(a => audio = a);
+
+            var controller = CreateController();
+            controller.MountAudio(audio);
+            controller.Play();
+            Assert.IsTrue(controller.IsPlaying);
+            Assert.IsFalse(controller.IsPaused);
+            Assert.IsFalse(controller.IsStopped);
+            yield return new WaitForSeconds(audio.Duration / 1000f / 2f);
+            controller.Pause();
+            Assert.AreEqual(audio.Duration / 2f, controller.CurrentTime, Delta);
+            Assert.IsFalse(controller.IsPlaying);
+            Assert.IsTrue(controller.IsPaused);
+            Assert.IsFalse(controller.IsStopped);
+        }
+
+        [UnityTest]
+        public IEnumerator TestStop()
+        {
+            IAudio audio = null;
+            yield return LoadAudio(a => audio = a);
+
+            var controller = CreateController();
+            controller.MountAudio(audio);
+            controller.Play();
+            Assert.IsTrue(controller.IsPlaying);
+            Assert.IsFalse(controller.IsPaused);
+            Assert.IsFalse(controller.IsStopped);
+            yield return new WaitForSeconds(audio.Duration / 1000f / 2f);
+            controller.Stop();
+            Assert.AreEqual(0f, controller.CurrentTime, Delta);
+            Assert.IsFalse(controller.IsPlaying);
+            Assert.IsFalse(controller.IsPaused);
+            Assert.IsTrue(controller.IsStopped);
+        }
+
+        [UnityTest]
+        public IEnumerator TestSeek()
+        {
+            IAudio audio = null;
+            yield return LoadAudio(a => audio = a);
+
+            var controller = CreateController();
+            controller.MountAudio(audio);
+            controller.Play();
+            Assert.AreEqual(0f, controller.CurrentTime, Delta);
+            controller.Seek(500);
+            Assert.AreEqual(500f, controller.CurrentTime, Delta);
+        }
+
+        [UnityTest]
+        public IEnumerator TestSetVolume()
+        {
+            IAudio audio = null;
+            yield return LoadAudio(a => audio = a);
+
+            var controller = CreateController();
+            controller.MountAudio(audio);
+            controller.SetVolume(0.25f);
+            controller.Play();
+            yield return new WaitForSeconds(audio.Duration / 1000f);
+        }
+
+        [UnityTest]
+        public IEnumerator TestDispose()
+        {
+            IAudio audio = null;
+            yield return LoadAudio(a => audio = a);
+
+            var controller = CreateController();
+            controller.MountAudio(audio);
+            controller.Dispose();
+            try
+            {
+                controller.Play();
+                Assert.Fail();
+            }
+            catch (Exception) { }
+        }
+
+        private DummyController CreateController()
+        {
+            return new GameObject("controller").AddComponent<DummyController>();
+        }
+
+        private IEnumerator LoadAudio(Action<IAudio> onFinished)
+        {
+            new GameObject("Listener").AddComponent<AudioListener>();
+
+            Assert.IsNotNull(onFinished);
+            var request = new AudioRequest("file://" + Path.Combine(Application.streamingAssetsPath, "Audio/effect.mp3"));
+            request.OnFinishedResult += (audio) => onFinished(new UnityAudio(audio));
+            request.Start();
+            while(!request.IsFinished)
+                yield return null;
+            Assert.IsNotNull(request.Result);
+            AudioClip clip = request.Result as AudioClip;
+            Assert.IsNotNull(clip);
+            Assert.Greater(clip.length, 0f);
+        }
+
+        private class DummyController : UnityAudioController
+        {
+            private bool isPlaying;
+            private bool isPaused;
+
+            public override bool IsPlaying => isPlaying;
+
+            public override bool IsPaused => isPaused;
+
+            public override bool IsStopped => !isPlaying && !isPaused;
+
+
+            public override void Play(float delay)
+            {
+                base.Play(delay);
+                if (CanPlay())
+                {
+                    isPlaying = true;
+                    isPaused = false;
+                }
+            }
+
+            public override void Pause()
+            {
+                base.Pause();
+                if (CanPause())
+                {
+                    isPlaying = false;
+                    isPaused = true;
+                }
+            }
+
+            public override void Stop()
+            {
+                base.Stop();
+                if (CanStop())
+                {
+                    isPlaying = false;
+                    isPaused = false;
+                }
+            }
+        }
+    }
+}
