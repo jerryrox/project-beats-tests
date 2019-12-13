a93917a5c37b7a544b5714498435392dcad799e9
Jerry Kim <jerryrox@live.co.kr>
2019-12-13 18:24:07 +0900

    Added UguiInputBox test

---
+++ Framework/Graphics/UI/UguiInputBoxTest.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.TestTools;
+using PBFramework.Assets.Atlasing;
+using PBFramework.Graphics.Tests;
+using PBFramework.Dependencies;
+
+namespace PBFramework.Graphics.UI.Tests
+{
+    public class UguiInputBoxTest {
+        
+        [UnityTest]
+        public IEnumerator Test()
+        {
+            var dependencies = new DependencyContainer();
+            dependencies.CacheAs<IDependencyContainer>(dependencies);
+            dependencies.CacheAs<IAtlas<Sprite>>(new ResourceSpriteAtlas());
+
+            var env = GraphicTestEnvironment.Create();
+            var root = env.CreateRoot(dependencies);
+
+            var inputBox = root.CreateChild<UguiInputBox>("inputBox");
+            Assert.IsNotNull(inputBox.PlaceholderLabel);
+            Assert.IsNotNull(inputBox.ValueLabel);
+            Assert.IsNotNull(inputBox.Background);
+
+            inputBox.OnChanged += (value) =>
+            {
+                Assert.AreEqual(value, inputBox.Text);
+                Debug.Log("OnChanged: " + value);
+            };
+            inputBox.OnSubmitted += (value) =>
+            {
+                Assert.AreEqual(value, inputBox.Text);
+                Debug.Log("OnSubmitted: " + value);
+            };
+
+            inputBox.Font = env.ArialFont.ToFont();
+            inputBox.Background.Color = Color.black;
+
+            while (env.IsRunning)
+            {
+                UguiLabelTest.DoUpdateTest(inputBox, KeyCode.LeftShift);
+                DoUpdateTest(inputBox, KeyCode.LeftControl);
+                yield return null;
+            }
+        }
+
+        public static void DoUpdateTest(IInputBox inputBox, KeyCode holdKey = KeyCode.None)
+        {
+            if(holdKey != KeyCode.None && !Input.GetKey(holdKey))
+                return;
+
+            if(Input.GetKeyDown(KeyCode.Q))
+            {
+                inputBox.Placeholder = $"Placeholder {Random.Range(0, 1000)}";
+            }
+            if(Input.GetKeyDown(KeyCode.W))
+            {
+                if(inputBox.CharacterLimit > 0)
+                    inputBox.CharacterLimit = 0;
+                else
+                    inputBox.CharacterLimit = 10;                    
+            }
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                inputBox.SelectionColor = new Color(Random.Range(0.25f, 0.75f), Random.Range(0.25f, 0.75f), Random.Range(0.25f, 0.75f));
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                inputBox.LineType += 1;
+                if(inputBox.LineType > InputField.LineType.MultiLineNewline)
+                    inputBox.LineType = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                inputBox.InputType += 1;
+                if(inputBox.InputType > InputField.InputType.Password)
+                    inputBox.InputType = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                inputBox.KeyboardType += 1;
+                if(inputBox.KeyboardType > TouchScreenKeyboardType.Search)
+                    inputBox.KeyboardType = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                inputBox.ValidationType += 1;
+                if(inputBox.ValidationType > InputField.CharacterValidation.EmailAddress)
+                    inputBox.ValidationType = 0;
+            }
+        }
+    }
+}
--- Framework/Graphics/UI/UguiLabelTest.cs
+++ Framework/Graphics/UI/UguiLabelTest.cs
@@ -27,64 +27,66 @@ namespace PBFramework.Graphics.UI.Tests
             label.Font = font;
             label.Text = "This is my test text!";
 
-            int alignmentFloor = 0;
-            int alignmentIndex = 0;
             while (env.IsRunning)
             {
-                if (Input.GetKeyDown(KeyCode.Q))
-                {
-                    label.IsBold = !label.IsBold;
-                    Debug.Log("Bold: " + label.IsBold);
-                }
-                else if (Input.GetKeyDown(KeyCode.W))
-                {
-                    label.IsItalic = !label.IsItalic;
-                    Debug.Log("Italic: " + label.IsItalic);
-                }
-                else if (Input.GetKeyDown(KeyCode.E))
-                {
-                    label.WrapText = !label.WrapText;
-                    Debug.Log("Wrap text: " + label.WrapText);
-                }
-
-                if (Input.GetKeyDown(KeyCode.Minus))
-                {
-                    label.FontSize -= 2;
-                }
-                else if(Input.GetKeyDown(KeyCode.Equals))
-                {
-                    label.FontSize += 2;
-                }
-
-                if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
-                    alignmentFloor++;
-                    if(alignmentFloor >= 3)
-                        alignmentFloor = 0;
-                    label.Alignment = (TextAnchor)(alignmentFloor * 3 + alignmentIndex);
-                }
-                else if (Input.GetKeyDown(KeyCode.DownArrow))
-                {
-                    alignmentFloor--;
-                    if(alignmentFloor < 0)
-                        alignmentFloor = 2;
-                    label.Alignment = (TextAnchor)(alignmentFloor * 3 + alignmentIndex);
-                }
-                else if (Input.GetKeyDown(KeyCode.RightArrow))
-                {
-                    alignmentIndex++;
-                    if(alignmentIndex >= 3)
-                        alignmentIndex = 0;
-                    label.Alignment = (TextAnchor)(alignmentFloor * 3 + alignmentIndex);
-                }
-                else if (Input.GetKeyDown(KeyCode.LeftArrow))
-                {
-                    alignmentIndex--;
-                    if(alignmentIndex < 0)
-                        alignmentIndex = 2;
-                    label.Alignment = (TextAnchor)(alignmentFloor * 3 + alignmentIndex);
-                }
+                DoUpdateTest(label);
                 yield return null;
+            }
+        }
+
+        public static void DoUpdateTest(ILabel label, KeyCode holdKey = KeyCode.None)
+        {
+            if(holdKey != KeyCode.None && !Input.GetKey(holdKey))
+                return;
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                label.IsBold = !label.IsBold;
+                Debug.Log("Bold: " + label.IsBold);
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                label.IsItalic = !label.IsItalic;
+                Debug.Log("Italic: " + label.IsItalic);
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                label.WrapText = !label.WrapText;
+                Debug.Log("Wrap text: " + label.WrapText);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Minus))
+            {
+                label.FontSize -= 2;
+            }
+            else if(Input.GetKeyDown(KeyCode.Equals))
+            {
+                label.FontSize += 2;
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                label.Alignment += 3;
+                if(label.Alignment > TextAnchor.LowerRight)
+                    label.Alignment = (TextAnchor)((int)label.Alignment % ((int)TextAnchor.LowerRight + 1));
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                label.Alignment -= 3;
+                if(label.Alignment < TextAnchor.UpperLeft)
+                    label.Alignment += (int)TextAnchor.LowerRight + 1;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                label.Alignment += 1;
+                if(label.Alignment > TextAnchor.LowerRight)
+                    label.Alignment = (TextAnchor)((int)label.Alignment % ((int)TextAnchor.LowerRight + 1));
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                label.Alignment -= 1;
+                if(label.Alignment < TextAnchor.UpperLeft)
+                    label.Alignment += (int)TextAnchor.LowerRight + 1;
             }
         }
     }
