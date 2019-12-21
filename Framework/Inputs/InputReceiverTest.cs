c876aa5ef5a17188f6675b54e8ba85623653cf77
Jerry Kim <jerryrox@live.co.kr>
2019-12-21 11:44:35 +0900

    Updated IInputReceiver test for UGUI

--- Framework/Inputs/InputReceiverTest.cs
+++ Framework/Inputs/InputReceiverTest.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.TestTools;
 using PBFramework.Assets.Atlasing;
+using PBFramework.Graphics;
 using PBFramework.Graphics.UI;
 using PBFramework.Graphics.UI.Tests;
+using PBFramework.Graphics.Tests;
 using PBFramework.Dependencies;
 
 namespace PBFramework.Inputs.Tests
@@ -78,6 +80,121 @@ namespace PBFramework.Inputs.Tests
                 }
 
                 yield return null;
+            }
+        }
+
+        /*
+
+GUI Test scenario
+
+Root (0)
+- Obj (0)
+- Canvas (1)
+- - Obj (0)
+- Obj (4)
+- - Obj (1)
+- - - Obj (0)
+- - Obj (2)
+- Obj (5)
+- - Obj (100)
+
+        */
+
+        [UnityTest]
+        public IEnumerator TestUgui()
+        {
+            var inputManager = InputManager.Create(new Vector2(1280f, 720f));
+            var dependency = new DependencyContainer(true);
+            dependency.CacheAs<IInputManager>(inputManager);
+
+            var receivers = new DummyUguiObject[9];
+
+            Action<int> assertReceivedUntil = (int lastReceivedIndex) =>
+            {
+                for(int i=0; i<receivers.Length; i++)
+                {
+                    if(i <= lastReceivedIndex)
+                        Assert.IsTrue(receivers[i].DidUpdate);
+                    else
+                        Assert.IsFalse(receivers[i].DidUpdate);
+                }
+            };
+
+            var env = GraphicTestEnvironment.Create();
+            var root = env.CreateRoot(dependency);
+            {
+                var base0 = root.CreateChild<DummyUguiObject>("base0", 0);
+                {
+                    base0.SetReceiveInputs(true);
+                    receivers[8] = base0;
+                }
+                var base1 = root.CreateChild<DummyUguiObject>("base1", 1);
+                {
+                    base1.Canvas = base1.RawObject.AddComponent<Canvas>();
+                    base1.Canvas.overrideSorting = true;
+                    base1.Canvas.sortingOrder = 1;
+                    base1.SetReceiveInputs(true);
+                    receivers[1] = base1;
+
+                    var obj0 = base1.CreateChild<DummyUguiObject>("obj0", 0);
+                    {
+                        obj0.SetReceiveInputs(true);
+                        receivers[0] = obj0;
+                    }
+                }
+                var base2 = root.CreateChild<DummyUguiObject>("base2", 4);
+                {
+                    base2.SetReceiveInputs(true);
+                    receivers[7] = base2;
+
+                    var obj0 = base2.CreateChild<DummyUguiObject>("obj0", 1);
+                    {
+                        obj0.SetReceiveInputs(true);
+                        receivers[6] = obj0;
+
+                        var obj00 = obj0.CreateChild<DummyUguiObject>("obj00", 0);
+                        {
+                            obj00.SetReceiveInputs(true);
+                            receivers[5] = obj00;
+                        }
+                    }
+                    var obj1 = base2.CreateChild<DummyUguiObject>("obj1", 2);
+                    {
+                        obj1.SetReceiveInputs(true);
+                        receivers[4] = obj1;
+                    }
+                }
+                var base3 = root.CreateChild<DummyUguiObject>("base3", 5);
+                {
+                    base3.SetReceiveInputs(true);
+                    receivers[3] = base3;
+
+                    var obj0 = base3.CreateChild<DummyUguiObject>("obj0", 100);
+                    {
+                        obj0.SetReceiveInputs(true);
+                        receivers[2] = obj0;
+                    }
+                }
+            }
+
+            assertReceivedUntil(-1);
+            for (int i = 0; i < receivers.Length; i++)
+            {
+                yield return null;
+                Debug.Log("checking index: " + i);
+                assertReceivedUntil(i);
+                receivers[i].SetReceiveInputs(false);
+            }
+        }
+
+        private class DummyUguiObject : UguiObject
+        {
+            public bool DidUpdate { get; set; } = false;
+
+            public override bool ProcessInput()
+            {
+                DidUpdate = true;
+                return false;
             }
         }
 
