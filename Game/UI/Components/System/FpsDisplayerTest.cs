cba6c3b0e5619933b8969016c0c516a57feabf34
Jerry Kim <jerryrox@live.co.kr>
2020-04-22 17:08:39 +1200

    Create FpsDisplayerTest.cs

---
+++ Game/UI/Components/System/FpsDisplayerTest.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using PBGame.Tests;
+using PBGame.Graphics;
+using PBFramework.UI;
+using PBFramework.Graphics;
+using PBFramework.Dependencies;
+
+namespace PBGame.UI.Components.System.Tests
+{
+    public class FpsDisplayerTest {
+
+        private FpsDisplayer fpsDisplayer;
+
+        [ReceivesDependency]
+        private IRootMain RootMain { get; set; }
+        
+        
+        [UnityTest]
+        public IEnumerator Test()
+        {
+            yield return TestGame.Run(
+                this,
+                () => Init(),
+                Update
+            );
+        }
+        
+        private IEnumerator Init()
+        {
+
+            fpsDisplayer = RootMain.CreateChild<FpsDisplayer>("fps-displayer");
+            {
+                fpsDisplayer.Size = new Vector2(180f, 30f);
+            }
+            yield break;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+                SetFps(120);
+            if (Input.GetKeyDown(KeyCode.W))
+                SetFps(60);
+            if (Input.GetKeyDown(KeyCode.E))
+                SetFps(50);
+            if (Input.GetKeyDown(KeyCode.R))
+                SetFps(40);
+            if (Input.GetKeyDown(KeyCode.T))
+                SetFps(30);
+        }
+
+        private void SetFps(float fps)
+        {
+            Time.maximumDeltaTime = 1f / fps;
+            Application.targetFrameRate = (int)fps;
+        }
+    }
+}
