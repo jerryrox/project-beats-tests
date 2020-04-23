bcd180ecefdace39147a2339910a9c2bfbdd8514
Jerry Kim <jerryrox@live.co.kr>
2020-04-23 15:36:58 +1200

    Added testing for map sort types

--- Game/Networking/API/Osu/Requests/MapsetListTest.cs
+++ Game/Networking/API/Osu/Requests/MapsetListTest.cs
@@ -27,5 +27,29 @@ namespace PBGame.Networking.API.Osu.Requests.Tests
 
             Assert.IsTrue(response.Mapsets.All(m => m != null));
         }
+
+        [UnityTest]
+        public IEnumerator TestSorts()
+        {
+            var api = new OsuApi();
+
+            foreach (var sortType in (MapSortType[])Enum.GetValues(typeof(MapSortType)))
+            {
+                Debug.LogWarning("Requesting with sort type: " + sortType);
+                var request = new MapsetListRequest()
+                {
+                    Sort = sortType,
+                    IsDescending = true,
+                };
+
+                MapsetListResponse response = null;
+                request.OnRequestEnd += (r) => response = r;
+                api.Request(request);
+
+                while (response == null) yield return null;
+
+                Assert.IsTrue(response.Mapsets.All(m => m != null));
+            }
+        }
     }
 }
